a5a2dcdc00c69bec2b75252bb9ea1e62f3174107
aang7 <aang.drummer@gmail.com>
2016-06-28 15:51:13 -0500

    Implementing goHomeGauge test

---
+++ AttitudeGaugeTest-master/app/src/main/java/io/radika/bitmaptest/CustomView.java
@@ -0,0 +1,77 @@
+package io.radika.bitmaptest;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by greenapsis on 24/06/16.
+ */
+public class CustomView extends View {
+
+    private final Paint paint;
+    private float mPitch = 0; // Degrees
+    private float mRoll = 0; // Degrees, left roll is positive
+
+    // These are created once and reused in subsequent onDraw calls.
+    private Bitmap mSrcBitmap;
+    private Canvas mSrcCanvas;
+    private Bitmap mDstBitmap;
+
+    private int mWidth;
+    private int mHeight;
+
+    public CustomView(Context context) {
+        //super(context);
+        this(context,null);
+
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        //paint.setColor(R.color.md_green_600);
+        paint.setColor(0XFF259b24);
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w ;
+        mHeight = h;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        //super.onDraw(canvas);
+
+        int viewWidthHalf = getWidth()/2;
+        int viewHeightHalf = getHeight()/2;
+
+        int radius = 0;
+        if(viewWidthHalf>viewHeightHalf)
+            radius=(viewHeightHalf/2) + 69; // -28 , -10
+        else
+            radius=(viewWidthHalf/2) + 69;
+
+        //canvas.drawCircle(viewWidthHalf,viewHeightHalf,radius,paint);
+        canvas.drawArc(new RectF(viewWidthHalf-radius,viewHeightHalf-radius,viewWidthHalf+radius,viewHeightHalf+radius),mRoll+mPitch*radius/90,180-2*mPitch*radius/90,false,paint);//wArc(wingsCircleBounds, 0, 180, false, mMinPlanePaint);
+        Log.d("START :", Float.toString(mRoll));
+        Log.d("SWEEP :",Float.toString(180+mRoll));
+
+    }
+
+    public void setAttitude(float pitch, float roll) {
+        mPitch = pitch;
+        mRoll = roll;
+        invalidate();
+    }
+}
---
+++ AttitudeGaugeTest-master/app/src/main/java/io/radika/bitmaptest/MainActivity.java
@@ -0,0 +1,193 @@
+package io.radika.bitmaptest;
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.annotation.FloatRange;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Surface;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    CustomView customView;
+    int pitch, roll;
+
+    private GoHomeGauge gauge;
+    private int valor = 0;
+    private int distance = 0;
+
+    private SensorManager mSensorManager;
+    @Nullable
+    private Sensor mRotationSensor;
+    private WindowManager mWindowManager;
+
+    private TextView showValor;
+    private TextView showDistance;
+
+    private TextView showPitch;
+    private TextView showRoll;
+
+    //private String _pitch = "Pitch: ";
+    //private String _roll  = "Roll: ";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        customView = (CustomView)findViewById(R.id.cVIEW);
+        pitch = 0;
+        roll = 0;
+
+        mWindowManager = getWindow().getWindowManager();
+
+        mSensorManager = (SensorManager) getSystemService(Activity.SENSOR_SERVICE);
+        // Can be null if the sensor hardware is not available
+        mRotationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        gauge = (GoHomeGauge) findViewById(R.id.gaugeAtLocation);
+        gauge.setValue(valor, distance);
+
+        showValor = (TextView) findViewById(R.id.showValor);
+        showDistance = (TextView) findViewById(R.id.showDistance);
+        showPitch = (TextView) findViewById(R.id.showPitch);
+        showRoll = (TextView) findViewById(R.id.showRoll);
+
+        showValor.setText(Integer.toString(valor));
+        showDistance.setText(Integer.toString(distance));
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mSensorManager.registerListener(this, mRotationSensor, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void IncrementaD(View view){
+        distance +=5;
+        gauge.setValue(valor, distance);
+        showDistance.setText(Integer.toString(distance));
+    }
+
+    public void DecrementaD(View view){
+        distance -=5;
+        gauge.setValue(valor, distance);
+        showDistance.setText(Integer.toString(distance));
+
+    }
+
+    public void Incrementa(View view) {
+        valor += 10;
+        gauge.setValue(valor, distance);
+        showValor.setText(Integer.toString(valor));
+    }
+
+    public void Decrementa(View view) {
+        valor -= 10;
+        gauge.setValue(valor, distance);
+        showValor.setText(Integer.toString(valor));
+
+    }
+
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR)
+            updateOrientation(event.values);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    private void updateOrientation(float[] rotationVector) {
+        float[] rotationMatrix = new float[9];
+        SensorManager.getRotationMatrixFromVector(rotationMatrix, rotationVector);
+
+        final int worldAxisForDeviceAxisX;
+        final int worldAxisForDeviceAxisY;
+
+        // Remap the axes as if the device screen was the instrument panel,
+        // and adjust the rotation matrix for the device orientation.
+        switch (mWindowManager.getDefaultDisplay().getRotation()) {
+            case Surface.ROTATION_0:
+            default:
+                worldAxisForDeviceAxisX = SensorManager.AXIS_X;
+                worldAxisForDeviceAxisY = SensorManager.AXIS_Z;
+                break;
+            case Surface.ROTATION_90:
+                worldAxisForDeviceAxisX = SensorManager.AXIS_Z;
+                worldAxisForDeviceAxisY = SensorManager.AXIS_MINUS_X;
+                break;
+            case Surface.ROTATION_180:
+                worldAxisForDeviceAxisX = SensorManager.AXIS_MINUS_X;
+                worldAxisForDeviceAxisY = SensorManager.AXIS_MINUS_Z;
+                break;
+            case Surface.ROTATION_270:
+                worldAxisForDeviceAxisX = SensorManager.AXIS_MINUS_Z;
+                worldAxisForDeviceAxisY = SensorManager.AXIS_X;
+                break;
+        }
+
+        float[] adjustedRotationMatrix = new float[9];
+        SensorManager.remapCoordinateSystem(rotationMatrix, worldAxisForDeviceAxisX,
+                worldAxisForDeviceAxisY, adjustedRotationMatrix);
+
+        // Transform rotation matrix into azimuth/pitch/roll
+        float[] orientation = new float[3];
+        SensorManager.getOrientation(adjustedRotationMatrix, orientation);
+
+        // Convert radians to degrees
+        float pitch = orientation[1] * -57;
+        float roll = orientation[2] * -57;
+
+        Log.d("MAIN Pitch",Float.toString(pitch));
+        Log.d("MAIN Roll",Float.toString(roll));
+        customView.setAttitude(pitch, roll);
+
+        showPitch.setText(Float.toString(pitch));
+        showRoll.setText(Float.toString(roll));
+    }
+}
